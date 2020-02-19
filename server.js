6959dfc121d729b699c7aea0827a76e31b2554c7
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-02-19 00:23:23 -0300

    Servidor e regras de negócio

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const server = express();
+
+server.use(express.static('public'))
+
+server.use(express.urlencoded({ extend: true}))
+
+const Pool = require('pg').Pool
+const db = new Pool({
+  user: 'postgres',
+  password: 'b4tstut4',
+  host: 'localhost',
+  port: 5432,
+  database: 'Doe'
+})
+
+const nunjucks = require("nunjucks");
+nunjucks.configure("./", {
+  express: server
+})
+
+
+server.get("/", function(req, res){
+  db.query("SELECT * FROM donors", function(err, result){
+    if (err) return res.send("erro de banco de dados.")
+
+    const donors = result.rows
+    return res.render("index.html", { donors})
+  })
+  
+
+});
+
+server.post("/", function(req, res) {
+  const name = req.body.name
+  const email = req.body.email
+  const blood = req.body.blood
+
+  if (name == "" || email == "" || blood == "") {
+    return res.send("Todos os campos são obrigatórios.")
+  }
+
+  const query = `
+  INSERT INTO donors("name", "email", "blood") 
+  VALUES ($1, $2, $3) `
+
+  const values = [name, email, blood]
+  db.query(query, values, function(err){
+    if (err) return res.send("Erro no bando de dados.")
+    return res.redirect("/")
+  })
+})
+
+server.listen(3000);
